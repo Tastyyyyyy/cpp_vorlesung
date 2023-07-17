a64f9a8a6cc584ff2f3b9a39d9d652ea8c3b753a
kevilue <85792852+kevilue@users.noreply.github.com>
2023-07-17 14:49:10 +0200

    gtest update

---
+++ gtest_structure/test/gameTest.cpp
@@ -0,0 +1,22 @@
+#include "/lib/googletest/googletest/include/gtest/gtest.h"
+#include "../src/game.hpp"
+
+
+class GameTest
+    : public ::testing::Test
+{
+protected:
+    Game m_game;
+
+    virtual void SetUp() override
+    {
+        m_game = Game();
+    }
+
+};
+
+
+TEST_F(GameTest, ScoreIsZeroWithOnlyZeroRolls)
+{
+   EXPECT_EQ(0, 0);
+}
